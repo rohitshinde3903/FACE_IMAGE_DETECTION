02fac1b36e5516b2ff456e893ba509a014260e6b
rohitshinde3903 <rohitshinde3903@gmail.com>
2025-07-20 23:53:52 +0530

    Switch to gunicorn for Render deployment

--- app.py
+++ app.py
@@ -79,6 +79,5 @@ def upload():
     return render_template('result.html', prediction=label)
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run()
 
